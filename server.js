a27df89aa74bafdfe15ad511fb08ebe8268ce9f0
Sadeeq <sadiqajimustapha@gmail.com>
2019-03-19 00:27:47 +0100

    static file

--- server.js
+++ server.js
@@ -90,7 +90,8 @@ app.use('/watched', watched)
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('frontend/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../', 'frontend/build', 'index.html'));
+    console.log(path.resolve(__dirname, '../', 'frontend/build', 'index.html'));
   });
 }
 

d5e1256ee53c945fc53ae30c1ea30051ea1dfa28
Sadeeq <sadiqajimustapha@gmail.com>
2019-03-19 00:56:36 +0100

    client

--- server.js
+++ server.js
@@ -88,9 +88,9 @@ app.use('/cp', cp)
 app.use('/watched', watched)
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
 

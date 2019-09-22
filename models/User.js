2b8bb2b8f19070e36fbed8f4dace0ad625fbb00d
Sadiq Mustapha Aji <sadiqajimustapha@gmail.com>
2019-09-22 20:41:53 +0100

    All user

--- models/User.js
+++ models/User.js
@@ -50,4 +50,7 @@ const UserSchema = new Schema({
     }
 });
 
-module.exports = User = mongoose.model('users', UserSchema)
+module.exports = User = mongoose.model('users', UserSchema).schema.add({isBlocked: {
+    type: Boolean,
+    default: false
+}});
